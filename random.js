c8bbd56bbdcf9d905596962f7b3edcc90150fe23
嶋村元汰 <>
2024-07-03 14:09:17 +0900

    ＋ボタンを押した際の1/10の確率で背景が赤色になるようにするための試行錯誤

--- random.js
+++ random.js
@@ -1,9 +1,14 @@
-const Pachinko = () => {
-    const result = Math.random();
-    if(result < 0.1){
-        return "アタリ"
-    }else {
-        return "ハズレ"
-    }
-}
+(() => {
+    const $counter = document.getElementById("js-counter");
 
+    const Lottery = () => {
+        const result = Math.random();
+        if(result < 0.1){
+            $counter.style.backgroundColor = "#e60000";
+        }else{
+            $counter.style.backgroundColor = "#ffffff";
+        }
+    }
+
+    document.getElementsByClassName("js-button")[1].addEventListener("click", Lottery);
+})();
